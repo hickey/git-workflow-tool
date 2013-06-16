c2c691ad37c92eccbe0bed7a1236e95d99fb1f37
Gerard Hickey <hickey@kinetic-compute.com>
2013-06-16 05:11:20 -0700

    Added dynamic loading of command plugins

--- lib/gwt/command.rb
+++ lib/gwt/command.rb
@@ -1,2 +1,11 @@
 
-require 'gwt/command/feature'
+$LOAD_PATH.each do |dir|
+  if File.directory? "#{dir}/gwt"
+    Dir.foreach("#{dir}/gwt/command") do |file|
+      if file.end_with? '.rb'
+        require "gwt/command/#{File.basename(file, '.rb')}"
+      end
+    end
+  end
+end
+
