3e70cc94fda120c6172adf6213abd60ed8808371
Gerard Hickey <hickey@kinetic-compute.com>
2013-06-16 04:50:11 -0700

    Added feature subcommand

--- lib/gwt.rb
+++ lib/gwt.rb
@@ -1,5 +1,7 @@
 
 require 'rubygems'
+require 'json'
+
 require 'gwt/cli'
 require 'gwt/repo'
 require 'gwt/command'
@@ -11,14 +13,35 @@ module GWT
 
   @repo = GWT::Repo.open
   @config = {}
-  @repo.config.keys.grep(%r{^gwt\.}).each {|k| @config[k] = @repo.config(k)}
-
+  
+  # load the configuration into @config
+  keys = @repo.config.keys.grep(%r{^gwt\.})
+  unless keys.nil?
+    keys.each {|k| @config[k] = @repo.config(k)}
+  end
+  
+  
   module_function
   
   def repo;  @repo  end
   def config;  @config  end
+  def config_get(key)
+    val = @config[key]
+    unless val.nil?
+      if val.start_with? '['
+        val = JSON.parse val
+      end
+    end
+    val
+  end
   def config_set(key, val)
-    @repo.config(key, val)
+    if val.is_a? Array
+      puts "trying to save an array: #{val.inspect}"
+      puts "JSON = #{val.to_json}"
+      %x{git config #{key} '#{val.to_json}'}
+    else 
+      %x{git config #{key} #{val}}
+    end
     @config[key] = val
   end
   def config_del(key)
--- lib/gwt/command/feature.rb
+++ lib/gwt/command/feature.rb
@@ -7,13 +7,44 @@ module GWT
       module_function 
       
       def perform(repo, args)
-        puts "Running feature"
-        puts "repo = #{repo.inspect}"
-        puts "args = #{args.inspect}"
+        feature_cmd = args.shift
+        feature_name = args.shift
+        features = GWT::config_get('gwt.features').to_a
+        
+        case feature_cmd
+        when %r{create|add}
+          if feature_name.nil?
+            puts "Feature name not specified"
+            exit 2
+          end
+          repo.branch('master').checkout
+          repo.branch(feature_name).create
+          repo.branch(feature_name).checkout
+          features << feature_name
+          puts "features = #{features.inspect}"
+          
+          GWT::config_set('gwt.features', features)
+          
+        when %r{list}
+          unless features.nil?
+            features.each {|f| puts f}
+          end
+          
+        when %r{delete|del|remove|rm}
+         if feature_name.nil?
+            puts "Feature name not specified"
+            exit 2
+          end
+          repo.branch('master').checkout
+          repo.branch(feature_name).delete
+          features.delete(feature_name)
+          GWT::config_set('gwt.features', features)
+        
+        else
+          puts "Unknown subcommand"
+        end
+          
       end
-      
-      
-      
     end
   end
 end
