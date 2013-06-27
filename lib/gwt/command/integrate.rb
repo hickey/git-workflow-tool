3cea85f1290c4ecc5ab0b5ff4a47bae37567279b
Gerard Hickey <ghickey@ebay.com>
2013-06-26 22:32:53 -0700

    Testing integration

--- lib/gwt/command/integrate.rb
+++ lib/gwt/command/integrate.rb
@@ -26,6 +26,9 @@ HELP
         end
 
         # insure that we are clean
+        puts "changed = #{repo.status.changed}"
+        puts "added = #{repo.status.added}"
+        puts "deleted = #{repo.status.deleted}"
         unless repo.status.changed.nil? and repo.status.added.nil? and repo.status.deleted.nil?
           puts "Not all changes have been committed. Please commit all changes first."
           exit 2
